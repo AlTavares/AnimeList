714519950075d914984607164318b6ff10c540f5
Alexandre Mantovani Tavares <alexandre@live.in>
2019-02-05 13:34:20 -0200

    Initial Commit

---
+++ AnimeListTests/AnimeListTests.swift
@@ -0,0 +1,34 @@
+//
+//  AnimeListTests.swift
+//  AnimeListTests
+//
+//  Created by Alexandre Mantovani Tavares on 05/02/19.
+//  Copyright © 2019 Alexandre Mantovani Tavares. All rights reserved.
+//
+
+import XCTest
+@testable import AnimeList
+
+class AnimeListTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
